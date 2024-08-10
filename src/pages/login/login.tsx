872c4ebea1b4040a41641269ce3dec48da92132c
singhbraj <braj333singh@gmail.com>
2024-08-11 00:46:27 +0530

    add sign form

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,16 +1,57 @@
+import { Button, Card, Checkbox, Form, Input, Layout, Space } from "antd";
+import "./login.css";
+import { LockFilled, LockOutlined, UserOutlined } from "@ant-design/icons";
+import Logo from "../../components/icons/Logo";
 
 const LoginPage = () => {
   return (
-    <>
-    <div>Sign in</div>
-    <input placeholder="Username" />
-    <input placeholder="Password" />
-    <button>Log in</button>
-    <label htmlFor="remember-me">Remember me</label>
-    <input id="remember-me" type="checkbox" />
-    <a href="#">Forgot password</a>
-    </>
-  )
-}
+    <Layout className="loginwrapper">
+      <Space direction="vertical" align="center" size="large">
+        <Logo />
+        <Card
+          className="login-card"
+          bordered={false}
+          title={
+            <Space
+              style={{
+                width: "100%",
+                fontSize: "16",
+                justifyContent: "center",
+              }}
+            >
+              <LockFilled />
+              Sign in
+            </Space>
+          }
+        >
+          <Form>
+            <Form.Item name="username">
+              <Input prefix={<UserOutlined />} placeholder="Username" />
+            </Form.Item>
+            <Form.Item name="password">
+              <Input.Password
+                prefix={<LockOutlined />}
+                placeholder="Password"
+              />
+            </Form.Item>
+            <Form.Item name="remember">
+              <Checkbox>Remember me</Checkbox>
+              <a href="">Forgot password</a>
+            </Form.Item>
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                style={{ width: "100%" }}
+              >
+                Log in
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
+      </Space>
+    </Layout>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
