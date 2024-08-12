fa919dbc283dc22aeae9307f1df1f60b97031605
singhbraj <braj333singh@gmail.com>
2024-08-13 01:56:27 +0530

    add sidebar layout

--- src/layouts/dashboard.tsx
+++ src/layouts/dashboard.tsx
@@ -1,15 +1,129 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { NavLink, Navigate, Outlet } from "react-router-dom";
+import Icon from "@ant-design/icons";
 import { useAuthStore } from "../store";
+import { Layout, Menu, theme } from "antd";
+import { useState } from "react";
+import Logo from "../components/icons/Logo";
+import Home from "../components/icons/Home";
+import { foodIcon } from "../components/icons/FoodIcon";
+import BasketIcon from "../components/icons/BasketIcon";
+import GiftIcon from "../components/icons/GiftIcon";
+
+const { Sider, Header, Content, Footer } = Layout;
+
+// const getMenuItems = () => {
+const items = [
+  {
+    key: "/",
+    icon: <Icon component={Home} />,
+    label: <NavLink to="/">Home</NavLink>,
+  },
+
+  {
+    key: "/products",
+    icon: <Icon component={foodIcon} />,
+    label: <NavLink to="/products">Products</NavLink>,
+  },
+  {
+    key: "/orders",
+    icon: <Icon component={BasketIcon} />,
+    label: <NavLink to="/orders">Orders</NavLink>,
+  },
+  {
+    key: "/promos",
+    icon: <Icon component={GiftIcon} />,
+    label: <NavLink to="/promos">Promos</NavLink>,
+  },
+];
 
 const Dashboard = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  // call getself
   const { user } = useAuthStore();
+
   if (user === null) {
-    return <Navigate to="/auth/login" replace={true} />;
+    return (
+      <Navigate
+        to={`/auth/login?returnTo=${location.pathname}`}
+        replace={true}
+      />
+    );
   }
+  //   const items = getMenuItems(user.role);
+
   return (
     <div>
-      Dashboard
-      <Outlet />
+      <Layout style={{ minHeight: "100vh", background: colorBgContainer }}>
+        <Sider
+          collapsible
+          theme="light"
+          collapsed={collapsed}
+          onCollapse={(value) => setCollapsed(value)}
+        >
+          <div className="logo">
+            <Logo />
+          </div>
+
+          <Menu
+            theme="light"
+            defaultSelectedKeys={["/"]}
+            mode="inline"
+            items={items}
+          />
+        </Sider>
+        <Layout>
+          <Header
+            style={{
+              paddingLeft: "16px",
+              paddingRight: "16px",
+              background: colorBgContainer,
+            }}
+          >
+            {/* <Flex gap="middle" align="start" justify="space-between">
+              <Badge
+                text={
+                  user.role === "admin" ? "You are an admin" : user.tenant?.name
+                }
+                status="success"
+              />
+              <Space size={16}>
+                <Badge dot={true}>
+                  <BellFilled />
+                </Badge>
+                <Dropdown
+                  menu={{
+                    items: [
+                      {
+                        key: "logout",
+                        label: "Logout",
+                        onClick: () => logoutMutate(),
+                      },
+                    ],
+                  }}
+                  placement="bottomRight"
+                >
+                  <Avatar
+                    style={{
+                      backgroundColor: "#fde3cf",
+                      color: "#f56a00",
+                    }}
+                  >
+                    U
+                  </Avatar>
+                </Dropdown>
+              </Space>
+            </Flex> */}
+          </Header>
+          <Content style={{ margin: "24px" }}>
+            <Outlet />
+          </Content>
+          <Footer style={{ textAlign: "center" }}>Cravecart pizza shop</Footer>
+        </Layout>
+      </Layout>
     </div>
   );
 };
