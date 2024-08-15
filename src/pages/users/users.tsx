35761c3e97409f97d1d429a4ce962af058f4c732
singhbraj <braj333singh@gmail.com>
2024-08-15 23:44:35 +0530

    add user table

--- src/pages/users/users.tsx
+++ src/pages/users/users.tsx
@@ -1,10 +1,62 @@
+import { useQuery } from "@tanstack/react-query";
 import CommonBredcrum from "../../components/commanComponent/CommonBredcrum";
+import { getUsers } from "../../http/api";
+import { Table } from "antd";
+import { User } from "../../types";
 
+const columns = [
+  {
+    title: "ID",
+    dataIndex: "id",
+    key: "id",
+  },
+  {
+    title: "Name",
+    dataIndex: "firstName",
+    key: "firstName",
+    render: (_text: string, record: User) => {
+      return (
+        <div>
+          {record.firstName} {record.lastName}
+        </div>
+      );
+    },
+  },
+  {
+    title: "Email",
+    dataIndex: "email",
+    key: "email",
+  },
+  {
+    title: "Role",
+    dataIndex: "role",
+    key: "role",
+  },
+];
 const Users = () => {
+  const {
+    data: users,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["users"],
+    queryFn: () => {
+      return getUsers().then((res) => res.data);
+    },
+  });
+
   return (
-    <div>
+    <>
       <CommonBredcrum />
-    </div>
+      {isLoading && <div> Loading... </div>}
+      {isError && <div> {error.message} </div>}
+      <Table
+        style={{ marginTop: "20px" }}
+        columns={columns}
+        dataSource={users}
+      ></Table>
+    </>
   );
 };
 
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,18 @@ export type Credentials = {
   email: string;
   password: string;
 };
+
+export type Tenant = {
+  id: number;
+  name: string;
+  address: string;
+};
+
+export type User = {
+  id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  createdAt: string;
+  tenant: Tenant | null;
+};
