a9175c4fab4bda9b58d5ff8ca8ff20758ceede53
singhbraj <braj333singh@gmail.com>
2024-08-17 22:01:03 +0530

    fetch tenants

--- src/pages/users/forms/userForm.tsx
+++ src/pages/users/forms/userForm.tsx
@@ -1,61 +1,154 @@
-import { Card, Col, Form, Input, Row } from "antd";
+import { useQuery } from "@tanstack/react-query";
+import { Card, Col, Form, Input, Row, Select, Space } from "antd";
+import { getTenants } from "../../../http/api";
+import { Tenant } from "../../../types";
 
 const UserForm = () => {
+  const {
+    data: tenants,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["tenants"],
+    queryFn: () => {
+      return getTenants().then((res) => res.data);
+    },
+  });
+
+  //   console.log(tenants.tentantList);
+
   return (
     <Row>
       <Col span={24}>
-        <Card title="Basic Info">
-          <Form.Item label="First Name" name="firstName">
+        <Space direction="vertical" size="large">
+          <Card title="Basic Info" bordered={false}>
+            <Form.Item label="First Name" name="firstName">
+              <Row gutter={20}>
+                <Col span={12}>
+                  <Form.Item
+                    label="First name"
+                    name="firstName"
+                    rules={[
+                      {
+                        required: true,
+                        message: "First name is required",
+                      },
+                    ]}
+                  >
+                    <Input size="large" />
+                  </Form.Item>
+                </Col>
+                <Col span={12}>
+                  <Form.Item
+                    label="Last name"
+                    name="lastName"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Last name is required",
+                      },
+                    ]}
+                  >
+                    <Input size="large" />
+                  </Form.Item>
+                </Col>
+                <Col span={12}>
+                  <Form.Item
+                    label="Email"
+                    name="email"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Email is required",
+                      },
+                      {
+                        type: "email",
+                        message: "Email is not valid",
+                      },
+                    ]}
+                  >
+                    <Input size="large" />
+                  </Form.Item>
+                </Col>
+              </Row>
+            </Form.Item>
+          </Card>
+          <Card title="Security info" bordered={false}>
             <Row gutter={20}>
               <Col span={12}>
                 <Form.Item
-                  label="First name"
-                  name="firstName"
+                  label="Passoword"
+                  name="password"
                   rules={[
                     {
                       required: true,
-                      message: "First name is required",
+                      message: "Password required",
                     },
                   ]}
                 >
-                  <Input size="large" />
-                </Form.Item>
-              </Col>
-              <Col span={12}>
-                <Form.Item
-                  label="Last name"
-                  name="lastName"
-                  rules={[
-                    {
-                      required: true,
-                      message: "Last name is required",
-                    },
-                  ]}
-                >
-                  <Input size="large" />
-                </Form.Item>
-              </Col>
-              <Col span={12}>
-                <Form.Item
-                  label="Email"
-                  name="email"
-                  rules={[
-                    {
-                      required: true,
-                      message: "Email is required",
-                    },
-                    {
-                      type: "email",
-                      message: "Email is not valid",
-                    },
-                  ]}
-                >
-                  <Input size="large" />
+                  <Input size="large" type="password" />
                 </Form.Item>
               </Col>
             </Row>
-          </Form.Item>
-        </Card>
+          </Card>
+          <Card title="Role" bordered={false}>
+            <Row gutter={20}>
+              <Col span={12}>
+                <Form.Item
+                  label="Role"
+                  name="role"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Role is required",
+                    },
+                  ]}
+                >
+                  <Select
+                    id="selectBoxInUserForm"
+                    size="large"
+                    style={{ width: "100%" }}
+                    allowClear={true}
+                    onChange={() => {}}
+                    placeholder="Select role"
+                  >
+                    <Select.Option value="admin">Admin</Select.Option>
+                    <Select.Option value="manager">Manager</Select.Option>
+                  </Select>
+                </Form.Item>
+              </Col>
+              {/* {selectedRole === 'manager' && ( */}
+              <Col span={12}>
+                <Form.Item
+                  label="Restaurant"
+                  name="tenantId"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Restaurant is required",
+                    },
+                  ]}
+                >
+                  <Select
+                    size="large"
+                    style={{ width: "100%" }}
+                    allowClear={true}
+                    onChange={() => {}}
+                    placeholder="Select restaurant"
+                  >
+                    {tenants?.tentantList?.map((tenant: Tenant) => (
+                      <Select.Option value={tenant.id} key={tenant.id}>
+                        {tenant.name}
+                      </Select.Option>
+                    ))}
+                  </Select>
+                </Form.Item>
+              </Col>
+              {/* )} */}
+            </Row>
+          </Card>
+        </Space>
       </Col>
     </Row>
   );
